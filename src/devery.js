31cfbbdca414bc0a121d64d1a3d9b69eeb0d4f55
timofeymelnik <timofeymelnik@gmail.com>
2019-09-25 13:10:32 +0300

    Update erc721 address

--- src/devery.js
+++ src/devery.js
@@ -1,16 +1,16 @@
 import {EveToken, DeveryRegistry, DeveryERC721} from "@devery/devery";
 
 const registryAddress = '0x0364a98148b7031451e79b93449b20090d79702a';
-const erc721Address = '0xe587090839ddabeada37d07d34c2b3ff5e1ee2b620b2fbfed49bf433ba8d09a3';
+const erc721Address = '0x032ef0359eb068d3dddd6e91021c02f397afce5a';
 
 const checkAndUpdateAllowanceOfContract = contractAddress => async (account, minAllowance = 40, total = 100) => {
     try {
         const eveTokenClient = new EveToken();
-        const {provider} = eveTokenClient.getProvider();
         const currentAllowance = await eveTokenClient.allowance(account, contractAddress);
         if (parseFloat(currentAllowance.toString()) / 10e17 >= minAllowance) return;
 
         const {hash} = await eveTokenClient.approve(contractAddress, `${total}000000000000000000`);
+        const {provider} = eveTokenClient.getProvider();
         await provider.waitForTransaction(hash)
     } catch (e) {
         console.error(e)
